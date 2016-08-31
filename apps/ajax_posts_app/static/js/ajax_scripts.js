b2d885b044caded7bf78e6c76f6e40757eba7ae7
Phil Boothe <pfb3cn@virginia.edu>
2016-08-31 15:39:26 -0400

    corrected jquery url

--- apps/ajax_posts_app/static/js/ajax_scripts.js
+++ apps/ajax_posts_app/static/js/ajax_scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('form').submit(function(event) {
         event.preventDefault();
         $.ajax({
-            url: "{{ url 'posts' }}",
+            url: {% url 'posts' %},
             method: "post",
             data: $(this).serialize(),
             success: function(serverResponse) {
